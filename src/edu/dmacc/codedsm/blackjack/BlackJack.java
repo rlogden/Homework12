69e0083f558bcf41c3192b0646418551b7cf055a
Rachel Ogden <rlogden@dmacc.edu>
2019-03-23 14:13:34 -0500

    Blackjack homework first commit

--- src/edu/dmacc/codedsm/blackjack/BlackJack.java
+++ src/edu/dmacc/codedsm/blackjack/BlackJack.java
@@ -1,5 +1,6 @@
 package edu.dmacc.codedsm.blackjack;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -8,30 +9,50 @@ import java.util.Scanner;
 public class BlackJack {
 
     public static HashMap<String, List<Integer>> myDeck = new HashMap<String, List<Integer>>();
+    public static ArrayList<Card> dealerHand = new ArrayList<>();
     public static ArrayList<Card> playerHand = new ArrayList<>();
     public static ArrayList<Card> dealtCards = new ArrayList<>();
     public static String n;
     public static Scanner input = new Scanner(System.in);
+    public static String[] faceCardsHearts = {"Jack of Hearts", "Queen of Hearts", "King of Hearts", "Ace of Hearts"};
+    public static String[] faceCardsDiamonds = {"Jack of Diamonds", "Queen of Diamonds", "King of Diamonds", "Ace of Diamonds"};
+    public static String[] faceCardsClubs = {"Jack of Clubs", "Queen of Clubs", "King of Clubs", "Ace of Clubs"};
+    public static String[] faceCardsSpades = {"Jack of Spades", "Queen of Spades", "King of Spades", "Ace of Spades"};
+    public static ArrayList<Integer> faceCardsValues = new ArrayList<>();
+    public static ArrayList<Integer> aceValues = new ArrayList<>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         fillDeck(myDeck);
         firstDeal(myDeck, 2);
+        firstDealToDealer(myDeck, 2);
         playBlackjack();
     }
 
     public static List<Integer> createCards() {
         List<Integer> cards = new ArrayList<Integer>();
-        for (int i = 1; i < 14; i++) {
+        for (int i = 2; i <= 10; i++) {
             cards.add(i);
         }
         return cards;
     }
 
     public static void fillDeck(HashMap<String, List<Integer>> x) {
+        faceCardsValues.add(10);
         x.put("Hearts", createCards());
         x.put("Diamonds", createCards());
         x.put("Spades", createCards());
         x.put("Clubs", createCards());
+        for (int i = 0; i < 3; i++) {
+            x.put(faceCardsHearts[i], faceCardsValues);
+            x.put(faceCardsDiamonds[i], faceCardsValues);
+            x.put(faceCardsClubs[i], faceCardsValues);
+            x.put(faceCardsSpades[i], faceCardsValues);
+        }
+        aceValues.add(1);
+        x.put("Ace of Hearts", aceValues);
+        x.put("Ace of Diamonds", aceValues);
+        x.put("Ace of Clubs", aceValues);
+        x.put("Ace of Spades", aceValues);
     }
 
     public static Card dealCards(HashMap<String, List<Integer>> deck, int numOfCards) {
@@ -49,25 +70,94 @@ public class BlackJack {
         playerHand.addAll(dealtCards);
     }
 
-    public static void playBlackjack() {
+    public static void firstDealToDealer(HashMap<String, List<Integer>> x, int y) {
+        while (dealerHand.size() < 2) {
+            dealerHand.add(dealCards(x, y));
+        }
+        System.out.print("Dealer was dealt ");
+        dealerHandPrinter(dealerHand);
+        System.out.println(" and a card face down.");
+    }
+
+    public static void playBlackjack() throws IOException {
         boolean playing = true;
         while (playing) {
-            System.out.println("Your hand total is " + getSum(playerHand) + ". Would you like to hit? Enter 1 for hit, 2 for stand.");
-            n = input.next();
-            if (n.equals("1")) {
-                dealtCards.clear();
-                dealtCards.add(dealCards(myDeck, 1));
-                System.out.print("You were dealt ");
-                handPrinter(dealtCards);
-                playerHand.addAll(dealtCards);
-            } else if (n.equals("2")) {
-                System.out.printf("Player\'s hand was %d", getSum(playerHand));
-                System.out.print(" and included ");
+            if (getSum(playerHand) < 21) {
+                System.out.print("Your hand is ");
                 handPrinter(playerHand);
+                System.out.println("for a total of " + getSum(playerHand) + ". Would you like to hit? Enter 1 for hit, 2 for stand.");
+                n = input.next();
+                if (n.equals("1")) {
+                    dealtCards.clear();
+                    dealtCards.add(dealCards(myDeck, 1));
+                    System.out.print("You were dealt ");
+                    handPrinter(dealtCards);
+                    playerHand.addAll(dealtCards);
+                    if (getSum(dealerHand) < 17) {
+                        dealerHand.add(dealCards(myDeck, 1));
+                    }
+                } else if (n.equals("2")) {
+                    System.out.print("You decided to stay with ");
+                    handPrinter(playerHand);
+                    System.out.println(" for a total of " + getSum(playerHand) + ".");
+                    while (getSum(dealerHand) <= 16) {
+                        dealerHand.add(dealCards(myDeck, 1));
+                        System.out.print("The dealer chooses to hit and now has ");
+                        handPrinter(dealerHand);
+                        System.out.println(" for a total of " + getSum(dealerHand) + ".");
+                    }
+                    if (getSum(playerHand) > 21) {
+                        showHands();
+                        System.out.printf("%s wins!", "Dealer");
+                        writeHandsToFile(getSum(playerHand), getSum(dealerHand));
+                        playing = false;
+                    } else if (getSum(playerHand) > getSum(dealerHand)) {
+                        showHands();
+                        System.out.printf("%s wins!", "Player");
+                        writeHandsToFile(getSum(playerHand), getSum(dealerHand));
+                        playing = false;
+                    } else if ((getSum(playerHand) < getSum(dealerHand)) && (getSum(dealerHand) <= 21)) {
+                        showHands();
+                        System.out.printf("%s wins!", "Dealer");
+                        writeHandsToFile(getSum(playerHand), getSum(dealerHand));
+                        playing = false;
+                    } else if ((getSum(playerHand) < getSum(dealerHand)) && (getSum(dealerHand) > 21)) {
+                        showHands();
+                        System.out.printf("%s wins!", "Player");
+                        writeHandsToFile(getSum(playerHand), getSum(dealerHand));
+                        playing = false;
+                    } else if (getSum(playerHand) == getSum(dealerHand)) {
+                        showHands();
+                        System.out.printf("It\'s a tie!");
+                        writeHandsToFile(getSum(playerHand), getSum(dealerHand));
+                        playing = false;
+                    }
+                } else {
+                    System.out.print("Invalid input. Your hand is currently ");
+                    handPrinter(playerHand);
+                }
+            } else if (getSum(dealerHand) == 21 && getSum(playerHand) != 21) {
+                showHands();
+                System.out.printf("%s wins!", "Dealer");
+                writeHandsToFile(getSum(playerHand), getSum(dealerHand));
                 playing = false;
-            } else {
-                System.out.print("Invalid input. Your hand is currently ");
-                handPrinter(playerHand);
+            } else if (getSum(playerHand) > 21) {
+                showHands();
+                System.out.printf("%s wins!", "Dealer");
+                writeHandsToFile(getSum(playerHand), getSum(dealerHand));
+                playing = false;
+            } else if (getSum(playerHand) == 21 && getSum(dealerHand) != 21) {
+                if (getSum(playerHand) > getSum(dealerHand)) {
+                    showHands();
+                    System.out.printf("%s wins!", "Player");
+                    writeHandsToFile(getSum(playerHand), getSum(dealerHand));
+                    playing = false;
+                } else if (getSum(playerHand) == getSum(dealerHand)) {
+                    showHands();
+                    System.out.printf("It\'s a tie!");
+                    writeHandsToFile(getSum(playerHand), getSum(dealerHand));
+                    playing = false;
+                }
             }
         }
     }
@@ -84,6 +174,11 @@ public class BlackJack {
         }
     }
 
+    public static void dealerHandPrinter(ArrayList<Card> toBePrinted) {
+        Card card = toBePrinted.get(0);
+        System.out.print(card.suit + " - " + card.value);
+    }
+
     public static int getSum(ArrayList<Card> toBeAdded) {
         int sum = 0;
         for (int i = 0; i < toBeAdded.size(); i++) {
@@ -91,5 +186,37 @@ public class BlackJack {
             sum = sum + a.value;
         }
         return sum;
+    }
+
+    public static void writeHandsToFile(int playerSum, int dealerSum) throws IOException {
+        File file = new File("blackjack_log.txt");
+        PrintWriter outFile = new PrintWriter(file);
+        outFile.println("Player total: " + playerSum);
+        outFile.println("Dealer total: " + dealerSum);
+        if (playerSum == 21 && dealerSum != 21) {
+            outFile.printf("%s wins!", "Player");
+        } else if (dealerSum == 21 && playerSum != 21) {
+            outFile.printf("%s wins!", "Dealer");
+        } else if (playerSum > 21 && dealerSum <= 21) {
+            outFile.printf("%s wins!", "Dealer");
+        } else if (playerSum <= 21 && dealerSum > 21) {
+            outFile.printf("%s wins!", "Player");
+        } else if (playerSum > dealerSum) {
+            outFile.printf("%s wins!", "Player");
+        } else if (dealerSum > playerSum) {
+            outFile.printf("%s wins!", "Dealer");
+        } else if (dealerSum == playerSum) {
+            outFile.printf("It\'s a tie!");
+        }
+        outFile.close();
+    }
+
+    public static void showHands() {
+        System.out.print("The dealer had ");
+        handPrinter(dealerHand);
+        System.out.print(" for a total of " + getSum(dealerHand) + ". ");
+        System.out.print("You had ");
+        handPrinter(playerHand);
+        System.out.print(" for a total of " + getSum(playerHand) + ". ");
     }
 }
--- src/edu/dmacc/codedsm/blackjack/DeckRandomizer.java
+++ src/edu/dmacc/codedsm/blackjack/DeckRandomizer.java
@@ -6,15 +6,16 @@ import java.util.*;
 
 public class DeckRandomizer {
 
+    private static final Random random = new Random();
+
     public static List<Card> chooseRandomCards(Map<String, List<Integer>> deck, int numberOfCards) {
         List<Card> chosenCards = new ArrayList<>();
         Set<String> suits = deck.keySet();
-        Random random = new Random();
 
         for (int i = 0; i < numberOfCards; i++) {
-            Card chosenSuitAndCard = choseACard(deck, suits.toArray(new String[suits.size()])[random.nextInt(suits.size())], random);
+            Card chosenSuitAndCard = getCard(deck, suits);
             while (chosenCards.contains(chosenSuitAndCard)) {
-                chosenSuitAndCard = choseACard(deck, suits.toArray(new String[suits.size()])[random.nextInt(suits.size())], random);
+                chosenSuitAndCard = getCard(deck, suits);
             }
             chosenCards.add(chosenSuitAndCard);
         }
@@ -22,10 +23,35 @@ public class DeckRandomizer {
         return chosenCards;
     }
 
-    private static Card choseACard(Map<String, List<Integer>> deck, String chosenSuit, Random random) {
+    private static String getChosenSuit(Set<String> suits) {
+        int suitCount = suits.size();
+        return suits.toArray(new String[suitCount])[random.nextInt(suitCount)];
+    }
+
+    private static Card getCard(Map<String, List<Integer>> deck, Set<String> suits) {
+        String chosenSuit = getChosenSuit(suits);
         List<Integer> suitCards = deck.get(chosenSuit);
+
+        if (checkIfDeckIsExhausted(deck)) {
+            throw new RuntimeException("Deck is exhausted");
+        }
+
+        while (suitCards.isEmpty()) {
+            chosenSuit = getChosenSuit(suits);
+            suitCards = deck.get(chosenSuit);
+        }
+
         Integer chosenCard = suitCards.get(random.nextInt(suitCards.size()));
         return new Card(chosenSuit, chosenCard);
+    }
+
+    private static boolean checkIfDeckIsExhausted(Map<String, List<Integer>> deck) {
+        Collection<List<Integer>> values = deck.values();
+        int numOfCards = 0;
+        for (List<Integer> value : values) {
+            numOfCards = numOfCards + value.size();
+        }
+        return numOfCards == 0;
     }
 
 }
